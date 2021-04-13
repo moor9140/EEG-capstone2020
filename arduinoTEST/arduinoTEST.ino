d3ac95d21cda7a03beb8b7f9b242b3e4a0dfdc42
me <moor9140@vandals.uidaho.edu>
2021-04-12 18:05:19 -0700

    arduino update

--- arduinoTEST/arduinoTEST.ino
+++ arduinoTEST/arduinoTEST.ino
@@ -9,12 +9,12 @@ void setup() {
 
 void loop() {
   for (i = 0; i < numPins-1; i++){
-    Serial.print(analogRead(i+pinOffset));
-    //Serial.print(random(200,400));
+    //Serial.print(analogRead(i+pinOffset));
+    Serial.print(random(200,400));
     Serial.print(",");
   }
-  Serial.print(analogRead(pinOffset-1));
-  //Serial.print(random(200,400));
+  //Serial.print(analogRead(pinOffset-1));
+  Serial.print(random(200,400));
   //Serial.print(analogRead(14));
   
   Serial.print("\n");
