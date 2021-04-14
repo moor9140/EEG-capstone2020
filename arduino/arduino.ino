9e3e2df604dbcdff078e7218a9ff1bf6b766b379
me <moor9140@vandals.uidaho.edu>
2021-04-13 17:57:26 -0700

    arduino test

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,22 +1,36 @@
-int LED = LED_BUILTIN;
-int numPins = 8;
-int pinOffset = 14;
-int i;
+/* 
+ *  Prints the analog values from pins in a sequence
+ */
+int numPins = 8;  // How many pins are being printed?
+int pinOffset = 14;  // Where does first pin begin?
+int baudRate = 9600;  // See serial monitor for values to choose
+//bool TESTING = true;  // print random values if true
+bool TESTING = false;
+int delayVal = 0;  // how long between new lines?
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(baudRate);  
+  while (TESTING){
+      testloop();  
+  }
 }
 
 void loop() {
-  for (i = 0; i < numPins-1; i++){
-    Serial.print(analogRead(i+pinOffset));
-    //Serial.print(random(200,400));
+  for (int i = 0; i < numPins-1; i++){
+    Serial.print(analogRead(i+pinOffset));  
     Serial.print(",");
   }
   Serial.print(analogRead(pinOffset-1));
-  //Serial.print(random(200,400));
-  //Serial.print(analogRead(14));
-  
   Serial.print("\n");
-  //delay(120);
+  delay(delayVal);
+}
+
+void testloop(){
+  for (int i = 0; i < numPins-1; i++){
+    Serial.print(random(200,400));  
+    Serial.print(",");
+  }
+  Serial.print(random(200,400));
+  Serial.print("\n");
+  delay(delayVal);  
 }
